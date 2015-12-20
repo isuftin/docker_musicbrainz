6b01eb73c51479b814e3966953b96a9a51b35f41
Suftin, Ivan <isuftin@usgs.gov>
2015-12-20 08:56:06 -0600

    Moved docker compose to root dir, added compose for server

--- musicbrainz-server/loaddb.sh
+++ musicbrainz-server/loaddb.sh
@@ -9,23 +9,28 @@ then
 
 # Download and import latest DB dump
 else
-  echo "Downloading latest DB dump..."
-  LATEST="$(curl -si ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/LATEST)"
-  echo "Latest DB dump found: $LATEST"
+     if [[ -f /dump/mbdump.tar.bz2 && -f /dump/mbdump-editor.tar.bz2 && -f /dump/mbdump-derived.tar.bz2 ]]; then
+         echo "Database dump files exist in /dump."
+     else
+         echo "Downloading latest DB dump..."
+         LATEST="$(curl -si ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/LATEST)"
+         echo "Latest DB dump found: $LATEST"
+         
+         # Download contents of latest dump folder
+         curl -s ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/${LATEST}/ | \
+           grep -e '^-' | awk '{ print $9 }' | \
+             while read f; do \
+               FILE="ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/${LATEST}/$f"; \
+               echo "Downloading ${FILE}..."; \
+               curl --create-dirs -o "/dump/${LATEST}/${f}" -C - ${FILE}; \
+             done
+             
+             # Verify data integrity
+             pushd /dump/${LATEST}/ && md5sum -c MD5SUMS && popd
+         
+             # Import database dumps
+             ./admin/InitDb.pl --createdb --import /dump/${LATEST}/mbdump*.tar.bz2 --echo
+         
+     fi
 
-  # Download contents of latest dump folder
-  curl -s ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/${LATEST}/ | \
-    grep -e '^-' | awk '{ print $9 }' | \
-      while read f; do \
-        FILE="ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/${LATEST}/$f"; \
-        echo "Downloading ${FILE}..."; \
-        curl --create-dirs -o "/tmp/${LATEST}/${f}" -C - ${FILE}; \
-      done
-  
-  # Verify data integrity
-  pushd /tmp/${LATEST}/ && md5sum -c MD5SUMS && popd
-
-  # Import database dumps
-  ./admin/InitDb.pl --createdb --import /tmp/${LATEST}/mbdump*.tar.bz2 --echo
-  
 fi
